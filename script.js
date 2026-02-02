f048c3e74e5eef2e35cd8cde951b1225a39eecad
BelliaNino <bellianino1996@gmail.com>
2026-02-02 15:58:12 +0100

    add age and distance

---
+++ script.js
@@ -0,0 +1,7 @@
+// Ask age and travel distance 
+const age = parseInt(prompt(`Type an age`))
+const distance = parseInt(prompt(`Choose distance in Km`))
+
+console.log(age);
+console.log(distance);
+
