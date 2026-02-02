97410651def58c21886a94baa780175a337768ba
BelliaNino <bellianino1996@gmail.com>
2026-02-02 16:12:10 +0100

    add  price

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ const distance = parseInt(prompt(`Choose distance in Km`))
 console.log(age);
 console.log(distance);
 
+// Km/euro
+const km = 0.21
+console.log(km);
+
+
+// amount per distance
+const price = (km * distance);
+console.log (price);
+
+
