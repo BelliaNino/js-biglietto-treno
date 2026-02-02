ebe9707ebccd92eb37b330f6eac53b781600c585
BelliaNino <bellianino1996@gmail.com>
2026-02-02 17:18:02 +0100

    fix total price

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 // Ask age and travel distance 
-const age = parseInt(prompt(`Type an age`))
+const age = parseInt(prompt(`Type your age`))
 const distance = parseInt(prompt(`Choose distance in Km`))
 
 console.log(age);
 console.log(distance);
 
-// Km/euro
+// this is the price in Euro per Km
 const km = 0.21
 console.log(km);
 
@@ -14,17 +14,17 @@ console.log(km);
 const price = (km * distance);
 console.log (price);
 
-// discount
+// discount criteria
 if(age < 18){
-
 
 message = `Get 20% off`
 total = price - (price * 0.2) 
-} else if (age > 64){
 
+} else if (age > 64){
 
 message = `Get 40% off`
 total = price - (price * 0.4)
+
 } else {
 
 message = `You don't have any discount`
@@ -32,6 +32,6 @@ total = price
 }
 
 console.log(message);
-console.log(total);
+console.log(total.toFixed(2));
 
 
